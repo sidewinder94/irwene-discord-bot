9f6f2bbb7c83c055ba441dcb7fc6a4dbb50f55a3
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2019-05-22 22:42:15 +0200

    Added Service project

--- irwene-discord-bot-aspcore/Services/DiscordBotService.cs
+++ irwene-discord-bot-aspcore/Services/DiscordBotService.cs
@@ -1,7 +1,12 @@
-﻿namespace irwene_discord_bot_aspcore.Services
+﻿using DiscordBot.Service;
+
+namespace irwene_discord_bot_aspcore.Services
 {
     public class DiscordBotService
     {
-        
+        public DiscordBotService()
+        {
+            var bot = new Class1();
+        }
     }
 }
